a2590533426161553fa51798658d3002cd84adc1
FHakomo <Zhila.vitaliy2016@gmail.com>
2025-12-26 14:36:20 +0200

    Створення контролера для виводу середовища

---
+++ src/CineVault.API/Controllers/AppInfoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineVault.API.Controllers;
+public class AppInfoController : Controller
+{
+    [HttpGet("api/environment")]
+    public IActionResult GetEnvironment()
+    {
+        // Отримуємо значення змінної середовища ASPNETCORE_ENVIRONMENT
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Unknown";
+        // Повертаємо значення у відповіді
+        return Ok(new { Environment = environment });
+    }
+
+}

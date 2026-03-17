535c74d6c364990c51b1705738c74d7410657909
FHakomo <Zhila.vitaliy2016@gmail.com>
2026-03-17 17:33:58 +0200

    Повністю виправлений AppInfoController

--- src/CineVault.API/Controllers/AppInfoController.cs
+++ src/CineVault.API/Controllers/AppInfoController.cs
@@ -1,15 +1,16 @@
 ﻿using Asp.Versioning;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CineVault.API.Controllers;
-public class AppInfoController : Controller
-{
-    [ApiVersion(1.0, Deprecated = true)]
-    [ApiVersion(2.0)]
-    [Route("api/v{version:apiVersion}/[controller]")]
 
-    [HttpGet("environment"), MapToApiVersion(1.0)]
+[ApiController]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion(1.0, Deprecated = true)]
+[ApiVersion(2.0)]
+public class AppInfoController : ControllerBase
+{
+    [HttpGet("environment")]
+    [MapToApiVersion(1.0)]
     public IActionResult GetEnvironmentV1()
     {
         // Отримуємо значення змінної середовища ASPNETCORE_ENVIRONMENT
@@ -18,12 +19,12 @@ public class AppInfoController : Controller
         return Ok(new { Environment = environment });
     }
 
-    [HttpGet("environment"), MapToApiVersion(2.0)]
+    [HttpGet("environment")]
+    [MapToApiVersion(2.0)]
     public IActionResult GetEnvironmentV2()
     {
-        // Отримуємо значення змінної середовища ASPNETCORE_ENVIRONMENT
+        // Той самий процес як і в попередньому, але з версією
         var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Unknown";
-        // Повертаємо значення у відповіді
         return Ok(new { Environment = environment, Response = "V2" });
     }
 }

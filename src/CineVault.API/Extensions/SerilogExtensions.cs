620a4738d4b21200fbce0564526ddc897be59d43
FHakomo <Zhila.vitaliy2016@gmail.com>
2026-03-23 11:20:17 +0200

    Створення SerilogExtensions та завантаження бібліотек

---
+++ src/CineVault.API/Extensions/SerilogExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+
+namespace CineVault.API.Extensions;
+
+public class SerilogExtensions
+{
+    public static IHostBuilder AddLogging(IHostBuilder hostBuilder)
+    {
+        Log.Logger = new LoggerConfiguration()
+         .MinimumLevel.Information()
+         .Enrich.FromLogContext()
+         .Enrich.WithMachineName()
+         .Enrich.WithThreadId()
+         .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}]{ Message: lj}{ NewLine}{ Exception}")
+         .WriteTo.File(          
+         path: "Logs/cinevault-zhila-vitalii-.txt",
+         rollingInterval: RollingInterval.Day,
+         outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Level:u3}]{ Message: lj}{ NewLine}{ Exception}")
+ .CreateLogger();
+
+        hostBuilder.UseSerilog();
+        return hostBuilder;
+    }
+}

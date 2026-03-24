924b2d88c65b91a6c86dd1c0d9e327958c31cbfc
FHakomo <Zhila.vitaliy2016@gmail.com>
2026-03-24 12:44:30 +0200

    Створення MappingConfig та встановлення Mapster

---
+++ src/CineVault.API/Controllers/Mappings/MappingConfig.cs
@@ -0,0 +1,49 @@
+﻿using Mapster;
+using CineVault.API.Controllers.Requests;
+using CineVault.API.Data.Entities;
+using CineVault.API.Controllers.Responses;
+namespace CineVault.API.Controllers.Mappings;
+
+public class MappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        // Конфігурація для Movie
+        config.NewConfig<MovieRequest, Movie>()
+            .Map(dest => dest.Title, src => src.Title)
+            .Map(dest => dest.Description, src => src.Description)
+            .Map(dest => dest.ReleaseDate, src => src.ReleaseDate)
+            .Map(dest => dest.Genre, src => src.Genre)
+            .Map(dest => dest.Director, src => src.Director);
+        config.NewConfig<Movie, MovieResponse>()
+            .Map(dest => dest.Id, src => src.Id)
+            .Map(dest => dest.Title, src => src.Title)
+            .Map(dest => dest.Description, src => src.Description)
+            .Map(dest => dest.ReleaseDate, src => src.ReleaseDate)
+            .Map(dest => dest.Genre, src => src.Genre)
+            .Map(dest => dest.Director, src => src.Director)
+            .Map(dest => dest.AverageRating, src => src.Reviews.Count != 0 ? src.Reviews.Average(r => r.Rating) : 0)
+            .Map(dest => dest.ReviewCount, src => src.Reviews.Count);
+        // Конфігурація для User
+        config.NewConfig<User, UserResponse>()
+            .Map(dest => dest.Id, src => src.Id)
+            .Map(dest => dest.Username, src => src.Username)
+            .Map(dest => dest.Email, src => src.Email);
+        config.NewConfig<UserRequest, User>()
+            .Map(dest => dest.Username, src => src.Username)
+            .Map(dest => dest.Email, src => src.Email)
+            .Map(dest => dest.Password, src => src.Password);
+
+        config.NewConfig<ReviewRequest, Review>()
+            .Map(dest => dest.Rating, src => src.Rating)
+            .Map(dest => dest.Comment, src => src.Comment);
+        config.NewConfig<Review, ReviewResponse>()
+            .Map(dest => dest.Id, src => src.Id)
+            .Map(dest => dest.MovieId, src => src.MovieId)
+            .Map(dest => dest.UserId, src => src.UserId)
+            .Map(dest => dest.Rating, src => src.Rating)
+            .Map(dest => dest.Comment, src => src.Comment)
+            .Map(dest => dest.CreatedAt, src => src.CreatedAt);
+
+    }
+}

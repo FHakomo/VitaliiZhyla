16cdd3bcb88aba89b65cba05cf694c7018093988
FHakomo <Zhila.vitaliy2016@gmail.com>
2026-03-27 19:03:53 +0200

    Додавання логічної довжини полів та обмеження на унікальні поля в CineVaultDbContext

--- src/CineVault.API/Data/Entities/CineVaultDbContext.cs
+++ src/CineVault.API/Data/Entities/CineVaultDbContext.cs
@@ -17,7 +17,51 @@ public sealed class CineVaultDbContext : DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-       modelBuilder.Entity<Comment>(entity => {             entity.HasOne(c => c.User)
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.Property(m => m.Title)
+                .IsRequired()
+                .HasMaxLength(300);
+            entity.Property(m => m.Description)
+                .HasMaxLength(10000);
+            entity.Property(m => m.Genre)
+                .HasMaxLength(100);
+            entity.Property(m => m.Director).HasMaxLength(200);
+            entity.HasIndex(m => m.Title);
+        });
+        modelBuilder.Entity<Review>(entity =>
+        {
+            entity.Property(r => r.Rating).IsRequired();
+            entity.Property(r => r.Comment).HasMaxLength(5000);
+             entity.HasOne(r => r.User)
+                .WithMany(u => u.Reviews)
+                .HasForeignKey(r => r.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+             entity.HasOne(r => r.Movie)
+                .WithMany(m => m.Reviews)
+                .HasForeignKey(r => r.MovieId)
+                .OnDelete(DeleteBehavior.Cascade);
+            entity.HasIndex(r => new { r.MovieId, r.UserId }).IsUnique();
+        });
+        modelBuilder.Entity<User>(entity =>
+        {           
+            entity.Property(entity => entity.Username)
+                .IsRequired()
+                .HasMaxLength(150);
+                entity.Property(entity => entity.Email)
+                .IsRequired()
+                .HasMaxLength(200);
+                entity.Property(entity => entity.Password)
+                .IsRequired()
+                .HasMaxLength(500);
+            entity.HasIndex(u => u.Username).IsUnique();
+            entity.HasIndex(u => u.Email).IsUnique();
+        });
+        
+        modelBuilder.Entity<Comment>(entity => {             entity.Property(c => c.Message)
+                .IsRequired()
+                .HasMaxLength(3000);
+            entity.HasOne(c => c.User)
                 .WithMany(u => u.Comments)
                 .HasForeignKey(c => c.UserId)
                 .OnDelete(DeleteBehavior.NoAction);
@@ -26,7 +70,29 @@ public sealed class CineVaultDbContext : DbContext
                 .HasForeignKey(c => c.ReviewId)
                 .OnDelete(DeleteBehavior.Cascade);
         });
+        modelBuilder.Entity<Actor>(entity =>
+        {
+            entity.Property(a => a.FullName)
+                .IsRequired()
+                .HasMaxLength(200);
+            entity.Property(a => a.Biography)
+                .HasMaxLength(5000);
+            entity.HasIndex(a => a.FullName);
+        });
+        modelBuilder.Entity<MovieActor>(entity =>
+        {
+            entity.HasKey(ma => new { ma.MovieId, ma.ActorId });
+            entity.HasOne(ma => ma.Movie)
+                .WithMany(m => m.MovieActors)
+                .HasForeignKey(ma => ma.MovieId)
+                .OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne(ma => ma.Actor)
+                .WithMany(a => a.MovieActors)
+                .HasForeignKey(ma => ma.ActorId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
         modelBuilder.Entity<CommentLike>(entity => {
+            entity.HasKey(ma => new { ma.CommentId, ma.UserId });
             entity.HasOne(cl => cl.User)
                 .WithMany(u => u.CommentLikes)
                 .HasForeignKey(cl => cl.UserId)
